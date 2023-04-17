d58490dca91b4fff51825b29efa8bd9ec371a8eb
spoletaev <info.elekit@yandex.ru>
2023-04-17 17:21:39 +0300

    добавить начальные настройки maven проекта

---
+++ src/main/java/ru/job4j/accidents/App.java
@@ -0,0 +1,14 @@
+package ru.job4j.accidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		System.out.println("Go to http://localhost:8080/index");
+	}
+
+}
