9f87c23037c60bf5442e82f53b31b4bfe3916c2b
spoletaev <info.elekit@yandex.ru>
2023-05-07 08:25:07 +0300

    добавить логирование

--- src/main/java/ru/job4j/accidents/App.java
+++ src/main/java/ru/job4j/accidents/App.java
@@ -1,14 +1,16 @@
 package ru.job4j.accidents;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-		System.out.println("Go to http://localhost:8080/index");
+		log.info("Web-service Accidents is started. Go to http://localhost:8080/index");
 	}
 
 }
